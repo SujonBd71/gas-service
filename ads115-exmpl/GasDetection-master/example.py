7e1ea43f763180f402e86a69824a3dfeae4919c5
sujon <sujon.sarkar31@gmail.com>
2021-09-19 17:38:51 -0400

    added mqtt

--- ads115-exmpl/GasDetection-master/example.py
+++ ads115-exmpl/GasDetection-master/example.py
@@ -2,9 +2,57 @@
 
 import time
 from gas_detection import GasDetection
+import json
+import paho.mqtt.client as mqtt
+
+
+temp_pub_topic = "kichen/sensors/gas/MQ2"
+
+def getPPM(ppm, detection):
+    payload=json.dumps(
+    {   "CO": ppm[detection.CO_GAS],
+        "H2": ppm[detection.H2_GAS],
+        "CH4":   ppm[detection.CH4_GAS],
+        "LPG": ppm[detection.LPG_GAS],
+        "PROPHANE": ppm[detection.PROPANE_GAS],
+        "ALCOHOL":   ppm[detection.ALCOHOL_GAS],
+        "SMOKE": ppm[detection.SMOKE_GAS]
+    }
+    )
+    
+    print(payload)
+    return payload
+
+
+def createMQTTClient():
+  Broker = "192.168.1.151"
+  hum_pub_topic = "living_room/weather/humidity"
+  
+  
+  def on_connect(client, userdata, flags, rc):
+      print("Connected with result code "+str(rc))
+  
+  def on_message(client, userdata, msg):
+      message = str(msg.payload)
+      print(msg.topic+" "+message)
+  
+  def on_publish(mosq, obj, mid):
+      print(mosq)
+  
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+  client.on_publish = on_publish
+  
+  client.connect(Broker, 1883, 60);
+  return client
+
+
 
 def main():
     """Handle example."""
+    client = createMQTTClient()
+    client.loop_start()
 
     print('Calibrating ...')
     detection = GasDetection()
@@ -12,16 +60,9 @@ def main():
     try:
         while True:
             ppm = detection.percentage()
-
-            print('CO: {} ppm'.format(ppm[detection.CO_GAS]))
-            print('H2: {} ppm'.format(ppm[detection.H2_GAS]))
-            print('CH4: {} ppm'.format(ppm[detection.CH4_GAS]))
-            print('LPG: {} ppm'.format(ppm[detection.LPG_GAS]))
-            print('PROPANE: {} ppm'.format(ppm[detection.PROPANE_GAS]))
-            print('ALCOHOL: {} ppm'.format(ppm[detection.ALCOHOL_GAS]))
-            print('SMOKE: {} ppm\n'.format(ppm[detection.SMOKE_GAS]))
-
-            time.sleep(0.25)
+            payload = getPPM(ppm, detection)
+            client.publish(temp_pub_topic, payload)
+            time.sleep(5)
 
     except KeyboardInterrupt:
         print('\nAborted by user!')
